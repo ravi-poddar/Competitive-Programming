9365fb2bfdea722a08b0958a834f035438843772
Ravi Kumar <57840416+ravi-poddar@users.noreply.github.com>
2020-12-08 22:13:06 +0530

    prime factorization in O(sqrt(n))

---
+++ NumberTheory/primeFactorization.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+void primeFact(int n){
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            int cnt=0;
+            while(n%i==0){
+                cnt++;
+                n/=i;
+            }
+            cout<<i<<" ^ "<<cnt<<endl;
+        }
+    }
+    if(n>1)
+        cout<<n<<" ^ "<<1<<endl;
+}
+
+int main(){ 
+    int n;
+    cout<<"\nEnter a number:";
+    cin>>n;
+    cout<<"\nPrime Factorization of "<<n<<"is:\n";
+    primeFact(n);
+    return 0;
+}
